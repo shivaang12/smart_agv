20a139aa534e20ae61d005f630abc8425ede1b46
shivang <shivaang14@gmail.com>
2018-12-01 22:17:12 -0500

    [Task 2.2.4] Stub implemented SmartBOT class. - It includes header file and source file.

---
+++ include/smartAGV/smartBOT.hpp
@@ -0,0 +1,55 @@
+/********************************************************************
+ *   MIT License
+ *  
+ *   Copyright (c) 2018 Shivang Patel
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is
+ *  furnished to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ *  SOFTWARE.
+ ********************************************************************/
+
+/** @file smartBOT.hpp
+ *  @brief Definition of class SmartBOT
+ *
+ *  This file contains definitions of class SmartBOT which receives  
+ *  commands from console and executes the commands accordingly.
+ *
+ *  @author Shivang Patel
+*/
+
+#pragma once
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <smartAGV/agvservice.h>
+#include <smartAGV/action.hpp>
+
+class SmartBOT {
+ public:
+  void initialize(ros::NodeHandle &);
+  void agvCallBack(const std_msgs::String::ConstPtr &);
+  void agvService(
+    smartAGV::agvservice::Request &,
+    smartAGV::agvservice::Response &
+  );
+
+ private:
+  ros::Subscribe agvSub;
+  ros::Publisher agvPub;
+  ros::ServiceServer agvServer;
+  Action action;
+  ros::NodeHandle nodeHandle;
+};
