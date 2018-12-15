5fed27149481cb92fb385d472cffcc342c3c9717
shivang <shivaang14@gmail.com>
2018-12-15 13:04:04 -0500

    [Task 2.5.2] Implemented SmartBOT class.

--- src/smartBOT.cpp
+++ src/smartBOT.cpp
@@ -37,11 +37,70 @@
 #include <smartAGV/action.hpp>
 #include <smartAGV/smartBOT.hpp>
 
-void SmartBOT::initialize(ros::NodeHandle &Nh) {}
+void SmartBOT::initialize(ros::NodeHandle &n) {
+  ROS_INFO_STREAM("smartBot::initialized");
+  nodeHandle = n;
+  agvSub = nodeHandle.subscribe("/smartbot/command", 1000,
+                                &SmartBOT::agvCallback, this);
 
-void SmartBOT::agvCallback(const std_msgs::String::ConstPtr &strPtr) {}
+  agvPub = nodeHandle.advertise<std_msgs::String>("/smartbot/command", 1000);
 
-bool SmartBOT::agvService(
-    smartagv::agvservice::Request &srvReq,
-    smartagv::agvservice::Response &srvRes
-  ) {}
+  agvServer = nodeHandle.advertiseService("agvService",
+                                          &SmartBOT::agvService, this);
+
+  action.initialize(nodeHandle);
+
+  return;
+}
+
+void SmartBOT::agvCallback(const std_msgs::String::ConstPtr &msg) {
+  std::string cmd;
+  std::string args;
+  std::istringstream lineStream(msg->data.c_str());
+
+  // parse string message into command and arguments
+  getline(lineStream, cmd, ',');
+  getline(lineStream, args, ',');
+
+  // convert command and args to lower case
+  std::transform(cmd.begin(), cmd.end(), cmd.begin(), ::tolower);
+  std::transform(args.begin(), args.end(), args.begin(), ::tolower);
+
+  ROS_INFO_STREAM("ServiceBot::commandCallback cmd=" << cmd
+                                                     << " args=" << args);
+
+  if ((cmd.find("move to") != std::string::npos) ||
+      (cmd.find("go to") != std::string::npos)) {
+    if (args.empty()) {
+      args = cmd;
+    }
+    action.execute(Action::ACT_MOVETO, args);
+  } else if ((cmd.find("stop moving") != std::string::npos) ||
+             (cmd.find("abort") != std::string::npos) ||
+             (cmd.find("cancel") != std::string::npos)) {
+    action.execute(Action::ACT_STOPMOVETO);
+  } else {
+    ROS_INFO_STREAM("ServiceBot:: unknown action");
+  }
+
+  return;
+}
+
+bool SmartBOT::agvService(smartagv::agvservice::Request &req,
+                          smartagv::agvservice::Response &resp) {
+  std_msgs::String msg;
+
+  ROS_INFO_STREAM("ServiceBot::commandService: receive req = " << req.cmd << " "
+                                                               << req.arg);
+
+  std::stringstream ss;
+  ss << req.cmd << "," << req.arg;
+  msg.data = ss.str();
+
+  // send messages
+  agvPub.publish(msg);
+
+  resp.resp = "OK";
+
+  return true;
+}
